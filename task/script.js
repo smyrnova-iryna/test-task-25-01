86d7ca672f0d6a751b0c1a3c836626ffd09525c1
Smyrnova Iryna <iraowl1234@gmail.com>
2025-01-24 19:39:55 +0200

    add background parts colors

--- task/script.js
+++ task/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+const colors = [];
 function getPoint() {
     var u = Math.random();
     var v = Math.random();
@@ -26,7 +27,6 @@ class Sketch {
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 1))
     this.renderer.setSize(this.width, this.height);
     this.renderer.setClearColor(0x000000, 1); 
-
     this.container.appendChild(this.renderer.domElement);
 
     this.camera = new THREE.PerspectiveCamera(
@@ -63,10 +63,10 @@ class Sketch {
 
   addObjects() {
     this.material = new THREE.PointsMaterial({
-         size: 0.01, sizeAttenuation: true, alphaTest: 0.5, transparent: true 
+       size: 0.01, sizeAttenuation: true, alphaTest: 0.5, transparent: true, 
+       vertexColors: true
     })
       
-    
 
     this.geometry = new THREE.BufferGeometry()
       let vertices = []
@@ -74,13 +74,13 @@ class Sketch {
       for ( let i = 0; i < 10000; i ++ ) {
 
 					let p = getPoint();
-
 					vertices.push( 4*p.x, 4*p.y, 4*p.z );
+          colors.push(Math.random(), Math.random(), Math.random())
 
 				}
 
 				this.geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
+        this.geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( new Float32Array(colors), 3));
 
     this.plane = new THREE.Points(this.geometry, this.material);
     this.scene.add(this.plane);
